39da74b3a667467c2c68a7a88b7c8637fe55fdb6
qsparks <q.m.sparks@gmail.com>
2020-12-24 16:10:42 -0600

    consolidates all JS into one file

--- scripts/app.js
+++ scripts/app.js
@@ -2,9 +2,149 @@
 const minute = 60000;
 const $petSprite = $('#pet');
 const $display = $('#display');
+
+const deerPath = './images/deer'
+const ghostPath = './images/ghost'
+const pixiePath = './images/pixie'
+const griffinPath = './images/griffin'
+
+const spriteData = [
+    {
+        id: 'deer',
+        idle: [
+            `${deerPath}/idle/frame1.png`,
+            `${deerPath}/idle/frame2.png`,
+            `${deerPath}/idle/frame3.png`,
+            `${deerPath}/idle/frame4.png`,
+        ],
+        walk: [
+            `${deerPath}/walk/frame1.png`,
+            `${deerPath}/walk/frame2.png`,
+            `${deerPath}/walk/frame3.png`,
+            `${deerPath}/walk/frame4.png`,
+        ]
+    },
+    {
+        id: 'ghost',
+        idle: [
+            `${ghostPath}/idle/frame1.png`,
+            `${ghostPath}/idle/frame2.png`,
+            `${ghostPath}/idle/frame3.png`,
+            `${ghostPath}/idle/frame4.png`,
+        ],
+        walk: [
+            `${ghostPath}/walk/frame1.png`,
+            `${ghostPath}/walk/frame2.png`,
+            `${ghostPath}/walk/frame3.png`,
+            `${ghostPath}/walk/frame4.png`,
+        ]
+    },
+    {
+        id: 'pixie',
+        idle: [
+            `${pixiePath}/idle/frame1.png`,
+            `${pixiePath}/idle/frame2.png`,
+            `${pixiePath}/idle/frame3.png`,
+            `${pixiePath}/idle/frame4.png`,
+        ],
+        walk: [
+            `${pixiePath}/walk/frame1.png`,
+            `${pixiePath}/walk/frame2.png`,
+            `${pixiePath}/walk/frame3.png`,
+            `${pixiePath}/walk/frame4.png`,
+        ]
+    },
+    {
+        id: 'griffin',
+        idle: [
+            `${griffinPath}/idle/frame1.png`,
+            `${griffinPath}/idle/frame2.png`,
+            `${griffinPath}/idle/frame3.png`,
+            `${griffinPath}/idle/frame4.png`,
+        ],
+        walk: [
+            `${griffinPath}/walk/frame1.png`,
+            `${griffinPath}/walk/frame2.png`,
+            `${griffinPath}/walk/frame3.png`,
+            `${griffinPath}/walk/frame4.png`,
+        ]
+    }
+]
+
 let game;
 
 // ANCHOR game classes
+class Carousel {
+    constructor(configObj) {
+        this.spriteData = configObj.sprites;
+        this.spriteObjects = [];
+        this.div = $('#carousel');
+        this.index = 0;
+
+        this.buildSprites();
+    }
+
+    buildSprites() {
+        this.spriteData.forEach(spriteObj => {
+            const sprite = new Sprite(spriteObj);
+            this.spriteObjects.push(sprite);
+            this.div.append(sprite.node);
+        })
+        this.setActiveSlide();
+    }
+
+    handleClick(buttonId) {
+        buttonId === 'forward' ? this.forward() : this.back();
+
+        this.setActiveSlide();
+    }
+
+    forward() {
+        if (this.index !== this.spriteObjects.length-1) {
+            this.index++;
+        } else {
+            this.index = 0;
+        }
+    }
+    
+    back() {
+        if (this.index !== 0) {
+            this.index--;
+        } else {
+            this.index = this.spriteObjects.length-1;
+        }
+    }
+
+    setActiveSlide() {
+        $('.active').removeClass('active');
+        $('.pet-option').eq(this.index).addClass('active');
+    }
+}
+
+class Sprite {
+    constructor(configObj) {
+        this.id = configObj.id;
+        this.idle = configObj.idle;
+        this.walk = configObj.walk;
+        this.index = 0;
+
+        this.buildNode();
+        this.animate(this.idle);
+    }
+
+    buildNode() {
+        this.node = $(`<img id="${this.id}" class="pet-option" src="#" alt="${this.id} sprite"></img>`);
+    }
+
+    animate(frameArr, interval=125) {
+        if (this.animation) clearInterval(this.animation);
+        this.animation = setInterval(() => {
+            this.node.attr('src', frameArr[this.index]);
+            this.index === frameArr.length - 1 ? this.index = 0 : this.index++;
+        }, interval);
+    }
+}
+
 class Tomagotchi {
     constructor(configObj) {
         // ANCHOR base tomagotchi stats
@@ -74,7 +214,9 @@ class Game {
         $('.game-info').toggleClass('hidden');
         $('.buttons').toggleClass('hidden');
         $('.stats').toggleClass('hidden');
-        $petSprite.addClass(this.pet.type);
+        this.sprite = characterSelect.spriteObjects[characterSelect.index];
+        this.sprite.node.removeClass('pet-option').removeClass('active').addClass('selected-pet');
+        $('#pet-container').append(this.sprite.node);
     }
 
     updateInfo() {
@@ -122,7 +264,17 @@ class Game {
 
 }
 
+// ANCHOR carousel instantiation
+const characterSelect = new Carousel({
+    sprites: spriteData
+})
+
 // ANCHOR event listeners
+
+$('.carousel-button').on('click', e => {
+    const direction = $(e.target).attr('id');
+    characterSelect.handleClick(direction);
+})
 
 $('#start-btn').on('click', (e) => {
     e.preventDefault();
